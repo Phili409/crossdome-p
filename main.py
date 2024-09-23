63aea54e306f89952e7908d992aa09876663b4d6
Phili409 <gabrielgalvez1221@gmail.com>
2024-09-23 15:39:12 -0500

    Update: main.py define program's entry point

---
+++ main.py
@@ -0,0 +1,14 @@
+# Import needed libraries
+import argparse
+from crossdome.core_functions import cross_compose
+from crossdome.utils import load_hla_database, save_results_to_csv
+from crossdome.visualization import plot_similarity_heatmap
+
+def main():
+    # Argument parser for command-line inputs? 
+    pass
+
+
+# Program entry point
+if __name__ == "__main__":
+    main()
