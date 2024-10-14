259af5ad08d478d9911b41fd9f4e5552b51ad1c4
Phili409 <gabrielgalvez1221@gmail.com>
2024-10-14 15:32:48 -0500

    Update: minor dependencies.

--- setup.py
+++ setup.py
@@ -16,12 +16,14 @@ setup(
         "numpy",
         "pandas",
         "matplotlib",
-        "seaborn"
+        "seaborn",
+        "pytest",
+        "scipy"
     ],
     classifiers=[  # Categorization
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  # Python version requirement
+    python_requires='>=3.12',  # Python version requirement
 )
